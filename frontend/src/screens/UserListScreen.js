0fc5acf6328ffb70f3c9d095c5beeeb471f7f935
Marvin Ramirez <eldo@hotmail.com>
2020-11-01 10:16:44 -0500

    admin screen access security

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -12,9 +12,16 @@ const UserListScreen = ({ history }) => {
   const userList = useSelector((state) => state.userList)
   const { loading, error, users } = userList
 
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+
   useEffect(() => {
-    dispatch(listUsers())
-  }, [dispatch])
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listUsers())
+    } else {
+      history.push('/login')
+    }
+  }, [dispatch, history, userInfo])
 
   const deleteHandler = (id) => {
     console.log('delete');
